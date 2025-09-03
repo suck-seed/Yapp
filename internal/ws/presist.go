a70ea8fde32aaf722b8227465461193517710e52
Sandesh <sandeshtiwari0211@gmail.com>
2025-09-03 15:55:47 +0545

    Trying lazygit
    yup

--- internal/ws/presist.go
+++ internal/ws/presist.go
@@ -6,6 +6,7 @@ import (
 	"github.com/suck-seed/yapp/internal/services"
 )
 
+// MakePresistFunc : Performs various actions and pushes it to db
 func MakePresistFunc(messageService services.IMessageService, userService services.IUserService) PersistFunc {
 	return func(ctx context.Context, in *InboundMessage) (*OutboundMessage, error) {
 
