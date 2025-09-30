5ec82fae263e962a4e9809436979a1b60ca6a535
Sandesh <sandeshtiwari0211@gmail.com>
2025-09-30 22:29:09 +0545

    Fix every issue, and everything works rn in this stage commit

--- internal/services/hall_service.go
+++ internal/services/hall_service.go
@@ -148,7 +148,7 @@ func (s *hallService) CreateHall(c context.Context, req *dto.CreateHallReq) (*dt
 		Description:      hall.Description,
 		CreatedAt:        hall.CreatedAt,
 		UpdatedAt:        hall.UpdatedAt,
-		CreatedBy:        hall.Owner,
+		Owner:            hall.Owner,
 	}, nil
 }
 
